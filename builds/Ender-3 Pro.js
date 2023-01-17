58ebcc2a47679177c28109a2147ae2d8c8db6616
nbost130 <nbost130@gmail.com>
2023-01-17 16:26:08 -0500

    fixed errors

--- builds/Ender-3 Pro.js
+++ builds/Ender-3 Pro.js
@@ -29,32 +29,32 @@ module.exports = {
             ["PREHEAT_2_TEMP_BED",     70],
             ["PREHEAT_2_TEMP_CHAMBER", 35],
             ["PREHEAT_2_FAN_SPEED",   255]
-            //["S_CURVE_ACCELERATION"], Breaks build in Marlin-Auto-Build on Git
-            //["INDIVIDUAL_AXIS_HOMING_MENU"],
+            //"S_CURVE_ACCELERATION", Breaks build in Marlin-Auto-Build on Git
+            //"INDIVIDUAL_AXIS_HOMING_MENU",
             //BL Touch changes from https://3dprintscape.com/marlin-firmware-on-creality-board-complete-guide/
-            //["BLTOUCH"],
-            //["NOZZLE_TO_PROBE_OFFSET", [10, 10, 0]],
+            //"BLTOUCH",
+            //"NOZZLE_TO_PROBE_OFFSET", [10, 10, 0]],
 
-            //["RESTORE_LEVELING_AFTER_G28"],
-            //["Z_SAFE_HOMING"],
+            //"RESTORE_LEVELING_AFTER_G28",
+            //"Z_SAFE_HOMING",
             //If you have z-stop and bltouch probe both plugged in, make these changes as well.
-            //["Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN"],
-            //["USE_PROBE_FOR_Z_HOMING"],
+            //"Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN",
+            //"USE_PROBE_FOR_Z_HOMING",
 
             //UBL
-            //["AUTO_BED_LEVELING_UBL"],
-            //["G26_MESH_VALIDATION"],
-            //["EEPROM_SETTINGS"]
+            //"AUTO_BED_LEVELING_UBL",
+            //"G26_MESH_VALIDATION",
+            //"EEPROM_SETTINGS"
         ],
         disable: [
     "SHOW_CUSTOM_BOOTSCREEN",
-    "CUSTOM_STATUS_SCREEN_IMAGE" 
+    "CUSTOM_STATUS_SCREEN_IMAGE"
 ]
     },
     configuration_adv: {
       enable: [
-        ["BLTOUCH_DELAY", 500"],
-        "BLTOUCH_FORCE_SW_MODE",
+        ["BLTOUCH_DELAY", 500],
+        "BLTOUCH_FORCE_SW_MODE", //DON'T THINK I NEED THIS
         "BLTOUCH_HS_MODE",
         "PROBE_OFFSET_WIZARD",
         ["PROBE_OFFSET_WIZARD_START_Z", -4.0],
